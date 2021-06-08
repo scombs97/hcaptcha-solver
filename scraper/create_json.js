015cb51d9ab2e532aad3fca6abb7f17fbc1cafb7
scombs97 <61484087+scombs97@users.noreply.github.com>
2021-06-08 03:10:48 -0400

    Added Scraper, Cache Creation, and Firebase Emulator

---
+++ scraper/create_json.js
@@ -0,0 +1,36 @@
+import fs from 'fs'
+import util from 'util'
+
+const readdir = util.promisify(fs.readdir);
+
+const main = async() => {
+    let json = {}
+
+    try {
+        if (fs.existsSync('cache.json')) {
+            let rawdata = fs.readFileSync('cache.json')
+            json = JSON.parse(rawdata);
+        } else {
+            let data = JSON.stringify(json, null, 2);
+            fs.writeFileSync('cache.json', data);
+        }
+    } catch(err) {
+        console.log(err)
+    }
+
+    let classifications;
+    classifications = await readdir("./images");
+
+    for (let classification of classifications) {
+        let names = await readdir("./images/" + classification);
+        for (let name of names) {
+            let hashStr = name.split('.')[0]
+            json[hashStr] = classification
+        }
+    }
+
+    let jsonData = JSON.stringify(json, null, 2);
+    fs.writeFileSync('cache.json', jsonData);
+}   
+
+main()
